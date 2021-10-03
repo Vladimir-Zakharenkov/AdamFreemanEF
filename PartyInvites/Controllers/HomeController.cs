4222222c2f209645cc1e38665869d1680ed3af93
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-03 21:02:06 +0300

    Создаем контроллер HomeController.cs

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext context;
+        public HomeController(DataContext ctx) => context = ctx;
+
+
+        public IActionResult Index() => View();
+        public IActionResult Respond() => View();
+
+        [HttpPost]
+        public IActionResult Respond(GuestResponse response)
+        {
+            context.Responses.Add(response);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Thanks),
+                new
+                {
+                    Name = response.Name,
+                    WillAttend = response.WillAttend
+                });
+        }
+
+        public IActionResult Thanks(GuestResponse response) => View(response);
+
+        public IActionResult ListResponses() =>
+            View(context.Responses.OrderByDescending(r => r.WillAttend));
+    }
+}
