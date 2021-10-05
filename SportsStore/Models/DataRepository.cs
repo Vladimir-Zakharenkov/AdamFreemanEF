a127c4f8c3780b7b70aa16c36da756ab88f600b8
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-05 22:26:04 +0300

    Создаем файл DataRepository.cs

---
+++ SportsStore/Models/DataRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Models
+{
+    public class DataRepository : IRepository
+    {
+        private List<Product> data = new();
+
+        public IEnumerable<Product> Products => data;
+
+        public void AddProduct(Product product) => data.Add(product);
+    }
+}
