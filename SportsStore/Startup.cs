6b96d663842386d9a89e4a9a5ba0443795d3eda3
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-05 09:50:00 +0300

    Создаем проект SportsStore настраиваем MVC

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -1,29 +1,27 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace PartyInvites
+namespace SportsStore
 {
     public class Startup
     {
         public IConfiguration Configuration { get; set; }
-
-        public Startup(IConfiguration config) => Configuration = config;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("PartyInvites"));
-                options.EnableSensitiveDataLogging(true);
-            }
-            );
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -39,8 +37,8 @@ namespace PartyInvites
             app.UseRouting();
 
             app.UseEndpoints(endpoints => endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}"));
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"));
         }
     }
 }
