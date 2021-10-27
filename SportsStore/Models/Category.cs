74ce7ee10d54751853fe3c11cf5851883e5578cf
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-27 18:02:03 +0300

    Создаем миграцию и базу данных SportsStore

--- SportsStore/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SportsStore.Models
-{
-    public class Category
-    {
-        public long Id {  get; set; }
-        public string Name {  get; set; }
-        public string Description {  get; set; }
-    }
-}
