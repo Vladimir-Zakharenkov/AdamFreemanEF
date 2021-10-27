55d00f9a1b215d72cfee5b63ea8fb9a567b4f459
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-10-27 09:48:22 +0300

    Изменяем Datarepository для работы с EF

--- SportsStore/Models/DataContext.cs
+++ SportsStore/Models/DataContext.cs
@@ -9,6 +9,5 @@ namespace SportsStore.Models
         }
 
         public DbSet<Product> Products {  get; set; }
-        public DbSet<Category> Categories {  get; set; }
     }
 }
--- SportsStore/Models/DataRepository.cs
+++ SportsStore/Models/DataRepository.cs
@@ -5,27 +5,28 @@ namespace SportsStore.Models
 {
     public class DataRepository : IRepository
     {
-        private List<Product> data = new();
+        //private List<Product> data = new();
 
-        public IEnumerable<Product> Products => data;
-
-        public void AddProduct(Product product)
-        {
-            this.data.Add(product);
-        }
-        //private DataContext context;
-
-        //public DataRepository(DataContext ctx) => context = ctx;
-
-        //public IEnumerable<Product> Products => context.Products.ToArray();
-
-        //public Product GetProduct(long key) => context.Products.Find(key);
+        //public IEnumerable<Product> Products => data;
 
         //public void AddProduct(Product product)
         //{
-        //    context.Products.Add(product);
-        //    context.SaveChanges();
+        //    this.data.Add(product);
         //}
+
+        private DataContext context;
+
+        public DataRepository(DataContext ctx) => context = ctx;
+
+        public IEnumerable<Product> Products => context.Products;
+
+        //public Product GetProduct(long key) => context.Products.Find(key);
+
+        public void AddProduct(Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+        }
 
         //public void UpdateProduct(Product product)
         //{
