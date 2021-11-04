8bd20bc20018630f8a8e87a85143f9c575e188ef
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-11-04 20:43:14 +0300

    Настраиваем UpdateAll

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -42,11 +42,17 @@ namespace SportsStore.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        //[HttpPost]
-        //public IActionResult Delete(Product product)
-        //{
-        //    repository.Delete(product);
-        //    return RedirectToAction(nameof(Index));
-        //}
+        public IActionResult UpdateAll()
+        {
+            ViewBag.UpdateAll = true;
+            return View(nameof(Index), repository.Products);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateAll(Product[] products)
+        {
+            repository.UpdateAll(products);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- SportsStore/Models/DataRepository.cs
+++ SportsStore/Models/DataRepository.cs
@@ -30,31 +30,31 @@ namespace SportsStore.Models
 
         public void UpdateProduct(Product product)
         {
-            //    Product p = GetProduct(product.Id);
-            //    p.Name = product.Name;
-            //    p.Category = product.Category;
-            //    p.PurchasePrice = product.PurchasePrice;
-            //    p.RetailPrice = product.RetailPrice;
-            context.Products.Update(product);
+            Product p = GetProduct(product.Id);
+            p.Name = product.Name;
+            p.Category = product.Category;
+            p.PurchasePrice = product.PurchasePrice;
+            p.RetailPrice = product.RetailPrice;
+            // context.Products.Update(product);
             context.SaveChanges();
         }
 
-        //public void UpdateAll(Product[] products)
-        //{
-        //    //  context.Products.UpdateRange(products);
-        //    Dictionary<long, Product> data = products.ToDictionary(p => p.Id);
-        //    IEnumerable<Product> baseline = context.Products.Where(p => data.Keys.Contains(p.Id));
+        public void UpdateAll(Product[] products)
+        {
+            //context.Products.UpdateRange(products);
+            Dictionary<long, Product> data = products.ToDictionary(p => p.Id);
+            IEnumerable<Product> baseline = context.Products.Where(p => data.Keys.Contains(p.Id));
 
-        //    foreach (Product databaseProduct in baseline)
-        //    {
-        //        Product requestProduct = data[databaseProduct.Id];
-        //        databaseProduct.Name = requestProduct.Name;
-        //        databaseProduct.Category = requestProduct.Category;
-        //        databaseProduct.PurchasePrice = requestProduct.PurchasePrice;
-        //        databaseProduct.RetailPrice = requestProduct.RetailPrice;
-        //    }
-        //    context.SaveChanges();
-        //}
+            foreach (Product databaseProduct in baseline)
+            {
+                Product requestProduct = data[databaseProduct.Id];
+                databaseProduct.Name = requestProduct.Name;
+                databaseProduct.Category = requestProduct.Category;
+                databaseProduct.PurchasePrice = requestProduct.PurchasePrice;
+                databaseProduct.RetailPrice = requestProduct.RetailPrice;
+            }
+            context.SaveChanges();
+        }
 
         //public void Delete(Product product)
         //{
